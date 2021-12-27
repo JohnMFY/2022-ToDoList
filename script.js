81babe6104776d8d2ba9cfdaf8144de2692494d8
Jonathan Mallefroy <jonathan.mallefroy@gmail.com>
2021-12-27 17:46:11 +0100

    first batch

---
+++ script.js
@@ -0,0 +1,16 @@
+/*" Write your tasks here !*/
+let i = 0;
+let placeholder = "";
+const text = " Write your tasks here !";
+const speed = 150;
+
+function type(){
+    placeholder += text.charAt(i);
+    document.getElementById("placeholder-id").setAttribute("placeholder",placeholder);
+    i++;
+    setTimeout (type, speed);
+}
+
+type ();
+
+console.log(" Write your tasks here !".length)//24 characters
