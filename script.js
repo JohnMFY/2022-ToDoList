2418311747f73918c9e0e52a7f44fa2716b008b4
Jonathan Mallefroy <jonathan.mallefroy@gmail.com>
2022-01-03 15:57:50 +0100

    suffering

--- script.js
+++ script.js
@@ -1,59 +1,63 @@
 /*effect typewritter when landing on page for the placeholder in input
-j'aimerai bien looper cette marde lô ! */
+j'aimerai bien looper cette marde lô ! 
 let i = 0;
 let placeholder = "";
 const text = " Write your tasks here !";
 const speed = 150;
-
 function type(){
     placeholder += text.charAt(i);
     document.getElementById("placeholder-id").setAttribute("placeholder",placeholder);
     i++;
-    setTimeout (type, speed);
+    setTimeout (type, speed)
 }
-
 type ();
+*/
 
-console.log(" Write your tasks here !".length);//24 characters
+/************************************/
+//ADD NEW TODO//
+const addButton = document.querySelector('#add_btn')
+const inputText = document.querySelector('#newitem')
+const todolist = document.querySelector ('#todolist')
+
+const items = document.querySelectorAll('.item')
 
 
-/*CONST*/
-const addButton = document.querySelector('.add_btn');
-const inputText = document.querySelector('#placeholder-id'); // 3 consts pour ADD TODOS
-const todolist = document.querySelector('#todo');
-
-const items = document.querySelector('.item');
-const fail_btn = document.querySelector('.fail');
-const validate_btn = document.querySelector('.validate');
-
-/*ADD TODOS*/
-
-addButton.addEventListener('click', function(){
-    const newTodo = inputText.value //newTodo est maintenant = à ce que l'utilisateur ecris dans l'input
-    inputText.value = '' //remet la value à vide
-    console.log(newTodo)
+addButton.addEventListener('click', function (event) {
+    const taskName = inputText.value // = what the user write on the input
+    inputText.value = '' //put back the value to empty
+    console.log(taskName)
 
     const divItem = document.createElement('div')
     divItem.classList.add('item')
-    divItem.innerHTML =` 
-        <button class="validate"><i class="far fa-check-circle icon"></i></button>
-        <h4>${newTodo}</h4>
-        <button class="fail"><i class="far fa-times-circle icon"></i></button>
-    `// je génére un bloc dynamique
-
-    todolist.appendChild(divItem) //j'ajoute des taches
+    divItem.innerHTML = 
+    `
+    <button class="validate"><i class="far fa-check-circle icon"></i></button>
+    <h4>${taskName}</h4>
+    `
+    addCross(divItem)
+    todolist.appendChild(divItem)
 })
 
+for(let item of items){
+    addCross(item)
+}
+
+function addCross(item){
+    //<button class="fail"><i class="far fa-times-circle icon"></i></button>
+    const cross = document.createElement('button')
+    cross.classList.add('delete')
+    cross.innerHTML='<i class="far fa-times-circle icon">'
+    item.appendChild(cross)
+    cross.addEventListener('click', function(event) {
+        event.target.parentNode.remove()
+    })
+}
 //change color of background if validate or not
-validate_btn.onclick =
-changeColorValidate;
-function changeColorValidate(){
-    items.style.background = '#68b846';
-    items.style.color = 'black';
-}
-fail_btn.onclick =
-changeColorFail;
-function changeColorFail(){
-    items.style.background = '#d41e24';
-    items.style.color = 'white';
-}
+
+//passer par class
+
+//mettre un minimum du characters pour les tasks
+
+//creer un bouton qui te donne 5 task (loop)
+
+//option de modification
