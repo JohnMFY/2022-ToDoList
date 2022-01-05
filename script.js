3d0d388873a3deb248d93d5913768bce4d83e1c7
Jonathan Mallefroy <jonathan.mallefroy@gmail.com>
2022-01-05 16:55:50 +0100

    1998 footix forever

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /*effect typewritter when landing on page for the placeholder in input
-j'aimerai bien looper cette marde lô ! 
+j'aimerai bien looper cette marde lô ! PROBABLY setInterval()
 let i = 0;
 let placeholder = "";
 const text = " Write your tasks here !";
@@ -22,21 +22,21 @@ const todolist = document.querySelector ('#todolist')
 const items = document.querySelectorAll('.item')
 
 
-addButton.addEventListener('click', function (event) {
+addButton.addEventListener('click', testAdd)
+
+function testAdd(){
     const taskName = inputText.value // = what the user write on the input
     inputText.value = '' //put back the value to empty
     console.log(taskName)
 
     const divItem = document.createElement('div')
     divItem.classList.add('item')
-    divItem.innerHTML = 
-    `
-    <h4>${taskName}</h4>
-    `
+    divItem.innerHTML = `<h4>${taskName}</h4>`
     addValidation(divItem)
     addCross(divItem)
+    //addEdit(divItem)
     todolist.appendChild(divItem)
-})
+}
 
 /**************************************/
 //VALIDATION & DELETE BUTTON 100%
@@ -59,23 +59,50 @@ function addValidation(item){
 //DELETE BUTTON 90%
 function addCross(item){
     //<button class="delete"><i class="far fa-times-circle icon"></i></button>
+    
     const cross = document.createElement('button')
     cross.classList.add('delete')
     cross.innerHTML='<i class="far fa-times-circle icon">'
-    item.appendChild(cross)
+    item.appendChild(cross) 
+    
     cross.addEventListener('click', function(event) {
         this.parentElement.classList.add("not_valid")
         this.parentElement.classList.add("del_anim")
-        this.parentElement.remove()// trouver un moyen de delay pour que l'animation se fasse solution probably setTimeout()     
+
+        setTimeout(deleteTodo, 600);
+
+        function deleteTodo(){
+            cross.parentElement.remove()
+        } 
+
     })
+
+   
+
+}
+
+
+
+
+/*******************************************/
+//MODIFICATION OF TODO 10%
+
+function addEdit(){
+    const edition = document.createElement('button')
+    edition.classList.add('edit')
+    edit.innerHTML='<i class="fas fa-edit">'
+    item.appendChild(edition)
+    //edit.addEventListener('click', function(event){
+
+    //})
 }
 
 /************************************************/
 //BUTTON WHO ADD 5 TODOS 100%
 const X5 = document.querySelector('#add_5')
 X5.addEventListener('click', function(event){
-    const todos = [{name:'The'},{name:'Cake'},{name:'Is'},{name:'A'},{name:'Lie'}];//forEach() probably the solution
-    console.log(todos)
+    const todos = [{name:'The'},{name:'Cake'},{name:'Is'},{name:'A'},{name:'Lie'}];//forEach() probably the solution : IT WAS 😏
+    console.log(todos.length)
     todos.forEach(function(todo) {
         const divItem = document.createElement('div')
         divItem.classList.add('item')
@@ -87,24 +114,28 @@ X5.addEventListener('click', function(event){
 })
 
 /***********************************************/
-//MINIMUM OF 3 CHARACTERS FOR INPUT 75%
+//MINIMUM OF 3 CHARACTERS FOR INPUT 100%
 const input = document.getElementById("newitem")
 console.log(input)
+wordLength()
+console.log(wordLength)
 input.addEventListener("input", wordLength)
 function wordLength(){
+    
     const inputWordLength = input.value.length
     console.log(inputWordLength)
     const minLenght = 3
     if (inputWordLength < minLenght) {
         input.classList.add("not_valid")
-        //addButton.removeEventListener('click', true)
+        const addButton = document.querySelector('#add_btn')
+        console.log(addButton)
+        addButton.removeEventListener('click', testAdd )
     }
     if(inputWordLength == 0) {
         input.classList.remove("not_valid")
-        //this.nextSibling.removeEventListener('click', true)
+        addButton.removeEventListener('click', testAdd)
     } else if (inputWordLength >= minLenght) {
         input.classList.remove("not_valid")
+        addButton.addEventListener('click', testAdd)
     }
 }
-/*******************************************/
-//MODIFICATION OF TODO 0%
